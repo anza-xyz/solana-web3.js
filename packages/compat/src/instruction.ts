ff6b5d0809cb1b0b1fb221dcbb37d2c00b9d574a
Steven Luscher <steven.luscher@anza.xyz>
2024-12-15 15:37:50 +0000

    Remove comments

--- packages/compat/src/instruction.ts
+++ packages/compat/src/instruction.ts
@@ -4,18 +4,12 @@ import { TransactionInstruction } from '@solana/web3.js';
 import { fromLegacyPublicKey } from './address';
 
 export function fromLegacyTransactionInstruction(legacyInstruction: TransactionInstruction): IInstruction {
-    // Map data (Buffer -> Uint8Array)
     const data = legacyInstruction.data ? Uint8Array.from(legacyInstruction.data) : undefined;
-
-    // Map keys (pubkey -> address, isSigner/isWritable -> role)
     const accounts = legacyInstruction.keys.map(key => ({
-        address: fromLegacyPublicKey(key.pubkey), // Convert PublicKey to string
-        role: determineRole(key.isSigner, key.isWritable), // Map role
+        address: fromLegacyPublicKey(key.pubkey),
+        role: determineRole(key.isSigner, key.isWritable),
     }));
-
-    // Map programId (PublicKey -> Address)
     const programAddress = fromLegacyPublicKey(legacyInstruction.programId);
-
     return {
         accounts,
         data,
@@ -23,7 +17,6 @@ export function fromLegacyTransactionInstruction(legacyInstruction: TransactionI
     };
 }
 
-// Helper function to determine the role
 function determineRole(isSigner: boolean, isWritable: boolean): AccountRole {
     if (isSigner && isWritable) return AccountRole.WRITABLE_SIGNER;
     if (isSigner) return AccountRole.READONLY_SIGNER;
